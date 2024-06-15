c73e346aa6cfbd1630b91bc84bacc9ae5ac361dc
Davi Prottes <161786596+Rondp@users.noreply.github.com>
2024-06-15 00:28:59 -0300

    Add files via upload

---
+++ JS/scriptprojetos.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    // Adicione aqui a lógica para alternar o menu
+}
+
+function submitForm() {
+    // Exibe uma mensagem de sucesso quando o formulário é enviado
+    alert('Cadastro realizado com sucesso!');
+    // Redireciona para a página 'PaginaHome.html'
+    window.location.href = 'paginahome.html';
+}
